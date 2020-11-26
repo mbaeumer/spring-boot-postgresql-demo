4f6805fa1a8247f238bc41878dbb00d93bfe7a8e
Martin Bäumer <martin.baeumer@gmail.com>
2020-11-26 22:59:45 +0100

    removed files

--- part1/src/main/java/se/maeumer/springbootlab/postgresql/part1/Contact.java
+++ part1/src/main/java/se/maeumer/springbootlab/postgresql/part1/Contact.java
@@ -3,7 +3,6 @@ package se.maeumer.springbootlab.postgresql.part1;
 import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.Id;
-import javax.persistence.Table;
 
 @Entity
 public class Contact {
